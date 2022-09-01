aecf9535ab28fe2560019bbbeea753e6d1c99228
david-commit <david.ondiege@students.moringaschool.com>
2022-09-01 10:49:23 +0300

    Accept fetch from localhost

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,22 @@
-window.onload = function() {
-    fetch('https://api.trade.gov/gateway/v1/ita_faqs/search')
+window.onload = function () {
+  fetch("http://localhost:3000/questionanswer")
     .then((response) => response.json())
     .then((data) => {
-        let body = document.querySelector(".faqs")
-        console.log(data)
-    })
-    .catch(error => console.error(error))
-}
+      console.log(data);
+      console.log(data[0].answer);
+
+      // data.forEach((data) => {
+        let body = document.querySelector("#faqs");
+        let h4 = document.createElement("h4");
+        let p = document.createElement("p");
+
+        h4.textContent = data[0].question;
+        p.textContent = data[0].answer;
+
+        body.append(h4);
+        h4.append(p);
+
+        p.style.fontWeight = "400"
+      // })
+    });
+};
