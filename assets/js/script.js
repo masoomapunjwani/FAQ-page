3292128302898a25167723147aa0a806c05a8b35
david-commit <david.ondiege@students.moringaschool.com>
2022-09-01 16:16:29 +0300

    Corrected page appearance

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,20 @@ window.onload = function () {
   fetch("http://localhost:3000/questionanswer")
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
-      console.log(data[0].answer);
 
-      // data.forEach((data) => {
+      data.forEach((item) => {
         let body = document.querySelector("#faqs");
         let h4 = document.createElement("h4");
         let p = document.createElement("p");
 
-        h4.textContent = data[0].question;
-        p.textContent = data[0].answer;
+        h4.textContent = item.question;
+        p.textContent = item.answer;
 
         body.append(h4);
         h4.append(p);
-
-        p.style.fontWeight = "400"
-      // })
+        p.style.fontWeight = "400";
+      });
     });
 };
+
+
