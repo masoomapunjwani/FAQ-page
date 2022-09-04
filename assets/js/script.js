d6d90b6b2788b862c0c767c066b96f116cc2f173
david-commit <david.ondiege@students.moringaschool.com>
2022-09-04 09:32:42 +0300

    README creation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,73 @@
+
+// ====================
+// window.onload = function () {
+//   fetch("http://localhost:3000/questionanswer")
+//     .then((response) => response.json())
+//     .then((data) => {
+
+//       data.forEach((item) => {
+//         let body = document.querySelector("#faqs");
+//         let q = document.querySelector("#faqsquestion");
+//         let a = document.querySelector("#faqsanswer");
+//         let h4 = document.createElement("h4");
+//         let p = document.createElement("p");
+
+//         h4.textContent = item.question;
+//         p.textContent = item.answer;
+
+//         body.append(h4);
+//         body.appendChild(p);
+//         p.style.fontWeight = "400";
+//       });
+//     });
+// };
+// =====================
+
 window.onload = function () {
   fetch("http://localhost:3000/questionanswer")
     .then((response) => response.json())
     .then((data) => {
+      console.log(data);
 
-      data.forEach((item) => {
-        let body = document.querySelector("#faqs");
-        let h4 = document.createElement("h4");
-        let p = document.createElement("p");
-
-        h4.textContent = item.question;
-        p.textContent = item.answer;
-
-        body.append(h4);
-        h4.append(p);
-        p.style.fontWeight = "400";
-      });
+      document.querySelector(".q1").textContent = data[0].question;
+      document.querySelector(".a1").textContent = data[0].answer;
+      document.querySelector(".q2").textContent = data[1].question;
+      document.querySelector(".a2").textContent = data[1].answer;
+      document.querySelector(".q3").textContent = data[2].question;
+      document.querySelector(".a3").textContent = data[2].answer;
+      document.querySelector(".q4").textContent = data[3].question;
+      document.querySelector(".a4").textContent = data[3].answer;
+      document.querySelector(".q5").textContent = data[4].question;
+      document.querySelector(".a5").textContent = data[4].answer;
+      document.querySelector(".q5").textContent = data[5].question;
+      document.querySelector(".a6").textContent = data[5].answer;
+      document.querySelector(".q7").textContent = data[6].question;
+      document.querySelector(".a7").textContent = data[6].answer;
+      document.querySelector(".q8").textContent = data[7].question;
+      document.querySelector(".a8").textContent = data[7].answer;
+      document.querySelector(".q9").textContent = data[8].question;
+      document.querySelector(".a9").textContent = data[8].answer;
+      document.querySelector(".q10").textContent = data[9].question;
+      document.querySelector(".a10").textContent = data[9].answer;
+      document.querySelector(".q11").textContent = data[10].question;
+      document.querySelector(".a11").textContent = data[10].answer;
+      
     });
 };
 
+// ====> Accordion
+const items = document.querySelectorAll(".accordion button");
 
+function toggleAccordion() {
+  const itemToggle = this.getAttribute("aria-expanded");
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute("aria-expanded", "false");
+  }
+
+  if (itemToggle == "false") {
+    this.setAttribute("aria-expanded", "true");
+  }
+}
+
+items.forEach((item) => item.addEventListener("click", toggleAccordion));
